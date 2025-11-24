8e520954373a16ca6374a369c92480ee8297d5f0
xandreiAThome <xandrei37@gmail.com>
2025-11-24 15:09:53 +0800

    fix: docs for UPDATE

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,46 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, Length, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiPropertyOptional({ description: 'Username', maxLength: 50 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  username?: string;
+
+  @ApiPropertyOptional({ description: 'First name', maxLength: 40 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 40)
+  firstName?: string;
+
+  @ApiPropertyOptional({ description: 'Last name', maxLength: 40 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 40)
+  lastName?: string;
+
+  @ApiPropertyOptional({ description: 'City', maxLength: 50 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  city?: string;
+
+  @ApiPropertyOptional({ description: 'Country', maxLength: 100 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 100)
+  country?: string;
+
+  @ApiPropertyOptional({ description: 'Zip code', maxLength: 20 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 20)
+  zipcode?: string;
+
+  @ApiPropertyOptional({ description: 'Gender', maxLength: 6 })
+  @IsOptional()
+  @IsString()
+  @Length(1, 6)
+  gender?: string;
+}
